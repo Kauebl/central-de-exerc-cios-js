c1aa168f29d753a927402c4053e497f24a0fa296
Kaue <kauebarbosa235@gmail.com>
2025-04-03 21:51:40 -0300

    exercicios

---
+++ condicionais.js
@@ -0,0 +1,20 @@
+let numero = prompt("Digite um numero");
+
+if (numero > 0) {
+
+    console.log("Positivo.");
+
+} else {
+
+    if (numero < 0) {
+
+        console.log("Negativo");
+
+    } else {
+
+        console.log("e zero.")
+
+
+    }
+
+}
--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-alert("Hello World na tela!");
+// alert("Hello World na tela!");
 
-console.log("Hello World no console!");
+// console.log("Hello World no console!");
+
+let nome = "Kaue";
+const idade = 19;
+var cidade = "Sao Paulo";
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+
+console.log("Meu nome e: " + nome); //concatenaçao
+console.log(`Meu nome e: ${nome}`); //Interpolacao
+
+console.log(`Minha idade e: ${idade} anos`);
+console.log(`Minha idade e: ${idade} anos`);
+
+console.log(`Minha cidade e: ${cidade}`);
+console.log(`Minha cidade e: ${cidade}`);
+
+let texto = "Tenho 2 celulares"; //string
+let numero = "20"; //number
+
+console.log(texto);
+console.log(numero);
+
+
+console.log(typeof idade);
+
+let n1 = 10;
+let n2 = 10;
+
+let soma = n1 + n2;
+
+console.log(soma);
