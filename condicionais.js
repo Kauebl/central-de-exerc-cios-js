9e92f94f0d5e02201bdbfb8f81b51bd7dc27d5cc
Kaue <kauebarbosa235@gmail.com>
2025-04-04 21:50:56 -0300

    exercicio4/4

--- condicionais.js
+++ condicionais.js
@@ -1,20 +1,105 @@
-let numero = prompt("Digite um numero");
+function exercicio01() {
 
-if (numero > 0) {
+    
 
-    console.log("Positivo.");
+    let idade = prompt("Digite sua idade")
 
-} else {
+    if (idade >= 18) {
 
-    if (numero < 0) {
-
-        console.log("Negativo");
+        console.log("Voce e maior de idade");
 
     } else {
 
-        console.log("e zero.")
+        console.log("Voce e menor de idade");
 
 
     }
+}
+
+function exercicio02() {
+
+   
+
+    let numero = prompt("Digite um numero")
+
+    if (numero >= 0) {
+
+        console.log("O numero e positivo")
+
+    } else {
+
+        console.log("O numero e negativo")
+
+    }
+}
+
+function exercicio03() {
+
+
+    let nota = prompt("Digite a nota do aluno")
+
+    if (nota >= 60) {
+
+
+        alert("Aprovado")
+
+    } else {
+
+        alert("Reprovado")
+    }
+
 
 }
+
+
+
+function exercicio04() {
+
+   debugger;
+
+    let numero = prompt("Digite um numero")
+
+    if (numero >= 0) {
+
+        alert("O numero e positivo")
+
+    } else {
+
+        alert("O numero e negativo")
+
+        alert("O numero e zero")
+
+    }
+
+
+
+} 
+
+
+  
+
+function exercicio05() {
+
+    let idade = prompt("Digite sua idade")
+
+    if (idade  >= 0 && idade <= 12) {
+
+        
+
+    } else {
+
+        if () {
+
+
+
+        } else {
+
+
+        }
+
+    }
+
+
+
+}
+
